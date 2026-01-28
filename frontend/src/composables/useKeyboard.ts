609e81e50e4a84424ec788c95aadc54d5d9973ca
Chiroru76 <s.chiharu7.s7@gmail.com>
2026-01-28 15:58:59 +0900

    useKeyboardでキーイベントを処理

--- frontend/src/composables/useKeyboard.ts
+++ frontend/src/composables/useKeyboard.ts
@@ -1,3 +1,21 @@
-export function useKeyboard() {
+import { onMounted, onUnmounted } from "vue";
+
+type KeyHandlerMap = Record<string, () => void>;
+
+export function useKeyboard(handlers: KeyHandlerMap) {
+  const onKeydown = (event: KeyboardEvent) => {
+    const handler = handlers[event.key];
+    if (!handler) return;
+    handler();
+  };
+
+  onMounted(() => {
+    window.addEventListener("keydown", onKeydown);
+  });
+
+  onUnmounted(() => {
+    window.removeEventListener("keydown", onKeydown);
+  });
+
   return {};
 }
