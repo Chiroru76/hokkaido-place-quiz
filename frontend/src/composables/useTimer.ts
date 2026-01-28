7c791a9832b514c9d6278639028679fa216c83ea
Chiroru76 <s.chiharu7.s7@gmail.com>
2026-01-28 14:50:47 +0900

    useTimerで計測を開始

--- frontend/src/composables/useTimer.ts
+++ frontend/src/composables/useTimer.ts
@@ -1,9 +1,23 @@
 import { ref } from "vue";
 
 export function useTimer() {
+  // 経過時間（秒）
   const elapsedTime = ref(0);
 
+  // setInterval のIDを保持する変数
+  let intervalId: number | null = null;
+
+  // タイマー開始
+  const start = () => {
+    if (intervalId !== null) return;
+
+    intervalId = window.setInterval(() => {
+      elapsedTime.value += 1;
+    }, 1000);
+  };
+
   return {
-    elapsedTime
+    elapsedTime,
+    start,
   };
 }
